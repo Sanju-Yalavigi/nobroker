99d574946b121f93c0c700fdf74c694975eb34a5
sanju-yalavigi <sanjushankar0@gmail.com>
2024-01-29 23:01:48 +0530

    second commit

--- src/main/java/com/nobroker/service/OwnerPlanService.java
+++ src/main/java/com/nobroker/service/OwnerPlanService.java
@@ -3,6 +3,10 @@ package com.nobroker.service;
 import com.nobroker.entity.OwnerPlan;
 import com.nobroker.payload.OwnerPlanDto;
 
+import java.util.List;
+
 public interface OwnerPlanService {
-    public OwnerPlanDto createOwnerPlan(OwnerPlanDto ownerPlanDto);
+    public OwnerPlanDto createOwnerPlans(OwnerPlanDto ownerPlanDto);
+
+    List<OwnerPlanDto> getAllOwnerPlans();
 }
